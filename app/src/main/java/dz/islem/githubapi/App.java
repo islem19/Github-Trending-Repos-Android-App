9d9400b83b75853d2bbb0d29ca0c2fd39c1544d0
Abdelkader Sellami <sellislem@gmail.com>
2019-11-02 12:09:24 +0000

    Setup Layout of RecyclerView

--- app/src/main/java/dz/islem/githubapi/App.java
+++ app/src/main/java/dz/islem/githubapi/App.java
@@ -10,5 +10,6 @@ public class App extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
