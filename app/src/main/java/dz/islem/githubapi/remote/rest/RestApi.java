8b90445bb7124789bb2e3592749c6a64c06c5f9b
Abdelkader Sellami <abdelkader.sellami@cevital.com>
2020-01-02 10:25:11 +0000

    code refactoring: using RxJava

--- app/src/main/java/dz/islem/githubapi/remote/rest/RestApi.java
+++ app/src/main/java/dz/islem/githubapi/remote/rest/RestApi.java
@@ -3,6 +3,7 @@ package dz.islem.githubapi.remote.rest;
 import java.util.Map;
 
 import dz.islem.githubapi.models.RepoModel;
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
@@ -10,5 +11,5 @@ import retrofit2.http.QueryMap;
 public interface RestApi {
 
     @GET("/search/repositories")
-    Call<RepoModel> getRepos(@QueryMap Map<String, String> filter);
+    Observable<RepoModel> getRepos(@QueryMap Map<String, String> filter);
 }
